d5b3fe6bf4aa242772394c3c614b8aaa1ce1c370
Stefan Staub <sstaub@users.noreply.github.com>
2024-07-13 15:15:54 +0200

    Update IDToolsPico.ino

--- examples/IDToolsPIco/IDToolsPico.ino
+++ examples/IDToolsPIco/IDToolsPico.ino
@@ -6,9 +6,9 @@ void setup() {
 	}
 
 void loop() {
-	static uint8_t cid[16];
-	generateUUID(cid);
-	Serial.printf("UUID %s version %d\n", printUUID(cid), verifyUUID(cid));
+	static uint8_t uuid[16];
+	generateUUID(uuid);
+	Serial.printf("UUID %s version %d\n", printUUID(uuid), verifyUUID(uuid));
 	static uint8_t mac[6];
 	generateMAC(mac);
 	Serial.printf("MAC %s\n", printMAC(mac));
